7a741b28bab2e1f28c7b963e03578d44e80a3d72
josemgd <josemgaudel@gmail.com>
2024-10-04 23:07:43 -0400

    errores de guardados corregidos

--- 3-funciones.js
+++ 3-funciones.js
@@ -90,17 +90,23 @@ function suma(n) {
 
 // 5.2 Escribe una función que calcule la sucesión de Fibonacci de forma recursiva. La sucesión de Fibonacci es una serie de números que empieza por 0 y 1 y cada número es la suma de los dos anteriores. Por ejemplo: `fibonacci(6) -> 8`
 function fibonacci(n) {
-    // Tu código aquí
+    if (n<=1){
+        return n
+    } return fibonacci(n-1)+fibonacci(n-2) // Tu código aquí
 }
 
 // 5.3 Crea una función recursiva llamada `factorial` que calcule el factorial de un número. El factorial de n (n!) es el producto de todos los números enteros positivos menores o iguales a n.
 function factorial(n) {
-    // Tu código aquí
+    if (n===0 || n===1){
+        return 1
+    } return n*factorial(n-1) // Tu código aquí
 }
 
 // 5.4 Escribe una función recursiva que calcule la potencia de un número dado el exponente. Por ejemplo: `potencia(2, 3) -> 8`
 function potencia(base, exponente) {
-    // Tu código aquí
+    if (exponente<=0){
+        return 1
+    } return base*potencia(base,exponente-1)// Tu código aquí
 }
 
 // Exportar las funciones para que puedan ser probadas
