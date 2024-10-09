b99b0df2606f22c018a59c2b609649565a47f575
josemgd <josemgaudel@gmail.com>
2024-10-09 19:28:34 -0300

    arreglado

--- tarea_arrays.js
+++ tarea_arrays.js
@@ -2,9 +2,10 @@
 // 1.1 Crea una función llamada `procesarPedido` que recibe un array `pedido`.
 // Debes sacar el primer elemento (nombre del cliente), añadir "bebida" al inicio del array y añadir el nombre del cliente al final.
 function procesarPedido(pedido) {
- const nombre = pedido.shift()
- pedido.unshift(bebida)
- pedido.push(nombre) // Tu código aquí
+    const nombre = pedido.shift()
+    pedido.unshift("bebiba")
+    pedido.push(nombre)
+    return pedido // Tu código aquí
 }
 
 // 2. Iteración de Arrays en Javascript
@@ -14,31 +15,25 @@ function procesarPedido(pedido) {
         const pares = numeros.filter(function(numeros){
             return numeros % 2 === 0
         })
+        if(pares.length === 0){
+            let suma = 0
+            return suma
+        }else{
         const suma = pares.reduce((acumulador, elementoActual)=>{
             return acumulador + elementoActual
         }) // tu código aquí
-        return suma
+        return suma}
     }
 
 // 3. Buscar en Arrays
 // 3.1 Escribe una función llamada `palabrasTerminanConA` que reciba un array de palabras y devuelva `true` si todas terminan con "a", o `false` si al menos una no lo hace.
 function palabrasTerminanConA(palabras) {
-    let terminacona = false
-function separarPalabras(palabras) {
-  return palabras.map(palabra => palabra.split(''));
-}
-const resultado = separarPalabras(palabras);// separa cada palabra en arrays
-resultado.forEach(function (resultado, index, originalArray) {
- const ultimaletra = resultado.pop()
- if (ultimaletra === "a"){
-     terminacona = true
- }
-})
-if (terminacona === true){
-    return true
-    
-} else{
-    return false}  // Tu código aquí
+    for (let i = 0; i < palabras.length; i++) {
+        if (!palabras[i].endsWith('a') && !palabras[i].endsWith('A')) {
+            return false;
+        }
+    }
+    return true;
 }
 
 const palabras = ['casa', 'silla', 'mesa']
@@ -62,16 +57,15 @@ console.log(buscaPalabras(words, 'mundo')) // El resultado debe ser: ['javascrip
 // 5.1 Crea una función llamada `findJavaScript` que reciba una matriz de cadenas y devuelva la posición de "JavaScript".
 // Si no la encuentra, devuelve [-1, -1].
 function findJavaScript(matrix) {
+    let array =[-1,-1]
     for (let i = 0; i < matrix.length; i++) {
   for (let j = 0; j < matrix[i].length; j++) {
     if (matrix[i][j]==='JavaScript'){
         let array = [i,j]
         return array
-    }else{
-        let array =[-1,-1]
     }
   }
-}// Tu código aquí
+}return array// Tu código aquí
 }
 
 const matrix = [
@@ -87,13 +81,13 @@ const matrix = [
 // 6.1 Crea una función llamada `findMinMaxPages` que reciba un array de números (representando el número de páginas de libros).
 // La función debe devolver un array con dos posiciones: el índice del libro con menos páginas y el índice del libro con más páginas.
 function findMinMaxPages(books) {
-    let max=Math.max(...libros)
-    let mayor = libros.indexOf(max)
-    let min=Math.min(...libros)
-    let menos = libros.indexOf(min)
+    let max=Math.max(...books)
+    let mayor = books.indexOf(max)
+    let min=Math.min(...books)
+    let menos = books.indexOf(min)
     let array =[menos,mayor]
     return array // Tu código aquí
-}
+
 
 const libros = [350, 120, 540, 275, 390, 130, 670];
 const resultado = findMinMaxPages(libros);
